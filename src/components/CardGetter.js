fb75f38b03594279438300ac4cc4b164919c1f46
ametrey <aetcheverryrey@gmail.com>
2022-02-09 23:14:19 -0300

    cardgetter component and header created, added css style and logo

---
+++ src/components/CardGetter.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { TraerPersonaje } from "../services/api";
+import { Card, Button } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const CardGetter = () => {
+const [personaje, setPersonaje] = useState();
+
+CallAPI();
+
+async function CallAPI() {
+    const res = await TraerPersonaje();
+    setPersonaje(res);
+  }
+
+  if (personaje == null) {
+    return <div>No hay nada</div>;
+  } else {
+    return (
+      <div className="CardGet">
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={personaje.image} />
+        <Card.Body>
+          <Card.Title>{personaje.name}</Card.Title>
+          <Card.Text>
+            Especie: {personaje.species}
+            <br></br>
+            Genero: {personaje.gender}
+            <br></br>
+            Status: {personaje.status}
+          </Card.Text>
+          <Button variant="primary">Detalle</Button>
+        </Card.Body>
+      </Card>
+      </div>
+    );
+  }
+}
+
+export { CardGetter }
